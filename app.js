155a27d5582483153be6489b5760891940271ac4
Himasnhu Mehra <140074179+HimanshuHiitDeveloper@users.noreply.github.com>
2023-09-24 19:53:18 +0530

    newChanges

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config();
 const app = express()
 const path = require('path')
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // app.use(express.json())
 app.use(express.urlencoded({extended:false}))
--- connections/connection.js
+++ connections/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-let url = process.env.DB_URL;
+let url = process.env.DB_URL || "mongodb+srv://Gymapp:12345@cluster0.37gwfmi.mongodb.net/?retryWrites=true&w=majority" ;
 const mongoConnect= async()=>{
     mongoose.connect(url).then(()=>{
         console.log("connection Succful")
